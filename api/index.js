f25be5c2265a27583f0b32e6be9eb311111e2487
Pablo <somosioticos@gmail.com>
2021-02-21 18:08:46 -0300

    commit

--- api/index.js
+++ api/index.js
@@ -53,8 +53,6 @@ if (process.env.SSLREDIRECT == "true"){
   });
 }
 
-
-
 //Mongo Connection
 const mongoUserName = process.env.MONGO_USERNAME;
 const mongoPassword = process.env.MONGO_PASSWORD;
@@ -104,6 +102,79 @@ mongoose.connect(uri, options).then(
   }
 );
 
+
+
+
+//********************************************* */
+//*******    C A M A R I T A ****************** */
+//********************************************* */
+
+const WebSocket = require("ws");
+
+const WS_PORT = 65080;
+
+const wsServer = new WebSocket.Server({ port: WS_PORT }, () =>
+  console.log(`WS Server is listening at ${WS_PORT}`)
+);
+
+let connectedClients = [];
+let connectedCams = [];
+
+//Servidor atento a que se realice una
+//conexión
+wsServer.on("connection", (ws, req) => {
+  console.log("Connected");
+
+  ws.on("message", data => {
+    if (data.indexOf("WEB_CLIENT") !== -1) {
+      connectedClients.push(ws);
+      console.log("WEB_CLIENT ADDED");
+      console.log(connectedClients.length);
+      return;
+    }
+
+    if (data == "CAM_CLIENT") {
+      connectedCams.push(ws);
+      console.log("CAM CLIENT ADDED");
+      console.log(connectedCams.length);
+      return;
+    }
+
+    connectedClients.forEach((ws, i) => {
+      if (connectedClients[i] == ws && ws.readyState === ws.OPEN) {
+        ws.send(data);
+      } else {
+        connectedClients.splice(i, 1);
+        console.log("WEB_CLIENT DELETED");
+      }
+    });
+  });
+
+  ws.on("error", error => {
+    console.error("WebSocket error observed: ", error);
+  });
+});
+
+function sendClientsToCam() {
+  connectedCams.forEach((ws, i) => {
+    if (connectedCams[i] == ws && ws.readyState === ws.OPEN) {
+      ws.send(connectedClients.length.toString());
+      console.log(
+        "Cantidad de usuarios viendo la cámara   " + connectedClients.length
+      );
+    } else {
+      connectedCams.splice(i, 1);
+      console.log("CAM_CLIENT DELETED");
+    }
+  });
+
+  setTimeout(() => {
+    sendClientsToCam();
+    console.log("  " + connectedCams.length);
+  }, 1000);
+}
+
+sendClientsToCam();
 
 
 
